ed407cff9de964c956a4300a08734c5340916f2e
sairuthwik <sairuthwikmaheshwaram@gmail.com>
2025-02-28 22:01:26 +0530

    Fix: Ensure authslice.js is included

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import {login,logout} from "./store/authslice"
+import {login,logout} from "./store/authSlice"
 import authService from './appwrite/auth'
 import  {Header}  from './components/index'
 import {Footer} from './components/index'
